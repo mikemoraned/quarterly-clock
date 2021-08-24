491a848cc417b83a8ce8966ed659d7bcfd8a160d
Mike Moran <mike@houseofmoran.com>
2021-08-24 23:14:53 +0100

    add d3 node module and calculate whole weeks so far

--- d3/main.js
+++ d3/main.js
@@ -1,1 +1,6 @@
-console.log("Feep");
+import * as d3 from "d3";
+
+const startOfYear = d3.timeYear();
+const startOfDay = d3.timeDay();
+const wholeWeeksSoFar = d3.timeWeek.count(startOfYear, startOfDay);
+console.log(startOfYear, startOfDay, wholeWeeksSoFar);
--- d3/rollup.config.js
+++ d3/rollup.config.js
@@ -1,8 +1,10 @@
+import resolve from "@rollup/plugin-node-resolve";
+
 export default {
   input: "main.js",
   output: {
     file: "bundle.js",
     format: "iife",
   },
-  plugins: [],
+  plugins: [resolve()],
 };
