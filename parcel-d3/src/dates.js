9b7ca831ec41b5e39133cf0ed271629b3a05ac5a
Mike Moran <mike@houseofmoran.com>
2021-11-12 20:17:08 +0000

    use d3 in main

--- parcel-d3/src/dates.js
+++ parcel-d3/src/dates.js
@@ -1,3 +1,7 @@
+import * as d3 from "d3";
+
 export function sum(a, b) {
   return a + b;
 }
+
+export const startOfDay = d3.timeDay();
--- parcel-d3/src/main.js
+++ parcel-d3/src/main.js
@@ -1,4 +1,6 @@
 import { sum } from "./dates.js";
+import { startOfDay } from "./dates";
 
 console.log("hello sweet world");
 console.dir(sum(1, 3));
+console.dir(startOfDay);
