510e1e707ed80235f3e4334b6b9cf8263d954155
Mike Moran <mike@houseofmoran.com>
2021-11-13 15:44:40 +0000

    extract `drawGuides`

--- parcel-d3/src/draw.js
+++ parcel-d3/src/draw.js
@@ -1,13 +1,22 @@
-export function draw(model, svg) {
+export function draw(dataModel, svg) {
   const margin = 100;
+
   const clockRadius = Math.min(svg.width - margin, svg.height - margin) / 2.0;
 
+  const guidesModel = {
+    outerRadius: clockRadius,
+  };
+
+  drawGuides(guidesModel, svg);
+}
+
+function drawGuides(model, svg) {
   svg.selection
     .append("circle")
-    .attr("class", "scaffold")
+    .attr("class", "guide")
     .attr("cx", 0)
     .attr("cy", 0)
-    .attr("r", clockRadius)
+    .attr("r", model.outerRadius)
     .style("fill", "white")
     .style("stroke", "red");
 }
