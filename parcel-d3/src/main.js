9682728ead2afefa882cb1c85c189f109ae2235e
Mike Moran <mike@houseofmoran.com>
2021-11-16 23:13:03 +0000

    add example dates in main for easier visual tests

--- parcel-d3/src/main.js
+++ parcel-d3/src/main.js
@@ -3,6 +3,15 @@ import { svg } from "./container";
 import { draw } from "./draw";
 
 console.log("Quarterly Clock");
-const dataModel = modelForDate(new Date());
+const now = new Date();
+// const now = Date.parse("01 Jan 2021 00:00:00 GMT");
+// const now = Date.parse("31 Mar 2021 00:00:00 GMT");
+// const now = Date.parse("01 Apr 2021 00:00:00 GMT");
+// const now = Date.parse("30 Jun 2021 00:00:00 GMT");
+// const now = Date.parse("01 Jul 2021 00:00:00 GMT");
+// const now = Date.parse("30 Sep 2021 00:00:00 GMT");
+// const now = Date.parse("01 Oct 2021 00:00:00 GMT");
+// const now = Date.parse("31 Dec 2021 00:00:00 GMT");
+const dataModel = modelForDate(now);
 console.dir(dataModel);
 draw(dataModel, svg("container"));
