e8c2c59f955eee5b74c3108df3fb7cf3596f1321
Mike Moran <mike@houseofmoran.com>
2021-11-19 16:33:58 +0000

    remove original version and rollup d3 version

--- d3/modules/model.mjs
+++
@@ -1,4 +0,0 @@
-import * as d3 from "d3";
-
-export const dummy = true;
-export const startOfDay = d3.timeDay();
--- d3/spec/model.spec.mjs
+++
@@ -1,7 +0,0 @@
-import { startOfDay } from "../modules/model.mjs";
-
-describe("Model", function () {
-  it("should do a dummy thing", function () {
-    expect(startOfDay).toEqual(true);
-  });
-});
