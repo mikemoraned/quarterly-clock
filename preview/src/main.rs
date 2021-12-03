434f32748cf8ac27bfc45f0f2a967929528ee87e
Mike Moran <mike@houseofmoran.com>
2021-12-03 14:08:09 +0000

    version which can build and listen locally, but just responsds with "oops" to http://localhost:8080/screenshot.png, so implying that something is up with connecting to chrome
    ```
    docker build --tag preview --file Dockerfile .
    docker run -p 8080:8080 preview
    ```

--- preview/src/main.rs
+++ preview/src/main.rs
@@ -41,7 +41,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .service(screenshot)
     })
-    .bind("127.0.0.1:8080")?
+    .bind("0.0.0.0:8080")?
     .run()
     .await
 }
