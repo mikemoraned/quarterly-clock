786c0d9a1a9ea47fb77bd39f9a85af7b3744cd68
Mike Moran <mike@houseofmoran.com>
2021-12-18 16:41:50 +0100

    use `log::info!` instead of `println!`

--- preview/src/bin/server.rs
+++ preview/src/bin/server.rs
@@ -23,29 +23,29 @@ async fn hello() -> impl Responder {
 
 fn grab_screenshot() -> Fallible<Vec<u8>> {
     let browser = Browser::new(LaunchOptionsBuilder::default().build().unwrap())?;
-    println!("created browser");
+    log::info!("created browser");
     
     let tab = browser.wait_for_initial_tab()?;
-    println!("got tab");
+    log::info!("got tab");
 
     tab.navigate_to("https://quarterly.houseofmoran.io/")?;
-    println!("navigated");
+    log::info!("navigated");
     tab.wait_for_element("#container > svg")?;
-    println!("got SVG");
+    log::info!("got SVG");
     let wait_duration = time::Duration::from_millis(1000);
     thread::sleep(wait_duration);
-    println!("waited {:?}", wait_duration);
+    log::info!("waited {:?}", wait_duration);
  
     let png_data = tab.capture_screenshot(ScreenshotFormat::PNG, None, true)?;
-    println!("got png data");
-    println!("peek: {:02X?}", &png_data[0 .. 8]);
+    log::info!("got png data");
+    log::info!("peek: {:02X?}", &png_data[0 .. 8]);
 
     Ok(png_data)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    println!("setting up logging, next message should be from log");
+    log::info!("setting up logging, next message should be from log");
     let _ = SimpleLogger::init(LevelFilter::Info, Config::default());
     log::info!("logging setup completed");
     HttpServer::new(|| {
