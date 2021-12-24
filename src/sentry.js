023d7c4b872666cdf3bb21c5946493c0dd915410
Mike Moran <mike@houseofmoran.com>
2021-12-24 21:42:13 +0100

    disable generic console logging to Sentry
    - it is too verbose

--- src/sentry.js
+++ src/sentry.js
@@ -5,7 +5,7 @@ import { CaptureConsole } from "@sentry/integrations";
 export function init({ tracesSampleRate }) {
   Sentry.init({
     dsn: "https://795e380a18ab416d997409fa24fa79b3@o1074871.ingest.sentry.io/6074817",
-    integrations: [new Integrations.BrowserTracing(), new CaptureConsole()],
+    integrations: [new Integrations.BrowserTracing()],
 
     tracesSampleRate,
   });
