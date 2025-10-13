1d4b6ab9ffab47868407bb17136e70f49c51296a
Mike Moran <mike@houseofmoran.com>
2025-10-13 01:19:45 +0100

    - switch to axum server - make url an arg to `grab_screenshot` - use tracing-based logging consistently - put url into AppState - get url from `APP_URL` env variable - allow the `APP_URL` to be overridden when running in local Docker - remove actix-web server and related dependencies

--- preview/src/bin/screenshot.rs
+++ preview/src/bin/screenshot.rs
@@ -1,29 +1,30 @@
 use headless_chrome::{Browser, LaunchOptionsBuilder};
 use image::ImageReader;
 use preview::grab::grab_screenshot;
+use tracing::info;
 use std::io::Cursor;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     std::env::set_var("RUST_BACKTRACE", "1");
     
-    println!("setting up logging, next message should be from log");
-    env_logger::init();
-    log::info!("logging setup completed");
+    println!("setting up logging, next message should be from log (if log-level set to INFO)");
+    tracing_subscriber::fmt::init();
+    info!("logging setup completed");
 
     let browser = Browser::new(LaunchOptionsBuilder::default().sandbox(false).build().unwrap())?;
-    log::info!("created browser");
+    info!("created browser");
     
-    let png_data = grab_screenshot(&browser)?;
-    println!("got png data");
-    println!("peek: {:02X?}", &png_data[0 .. 8]);
+    let png_data = grab_screenshot(&browser, "https://quarterly.houseofmoran.io/")?;
+    info!("got png data");
+    info!("peek: {:02X?}", &png_data[0 .. 8]);
     
     let png_image = ImageReader::new(Cursor::new(png_data))
         .with_guessed_format()?
         .decode()?;
-    println!("parsed as png image");
+    info!("parsed as png image");
 
     png_image.save("screenshot.png")?;
-    println!("saved as png file");
+    info!("saved as png file");
 
     Ok(())
 }
