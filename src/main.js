a8e283e9cef4071790da0b0352361acae8b7b619
Mike Moran <mike@houseofmoran.com>
2025-10-14 23:30:32 +0100

    switch back to current day

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import { draw } from "./draw/draw";
 import "@fortawesome/fontawesome-free/css/all.css";
 
 console.log("Quarterly Clock");
-// const now = new Date();
+const now = new Date();
 // const now = Date.parse("01 Jan 2022 00:00:00 GMT");
 // const now = Date.parse("01 Feb 2022 00:00:00 GMT");
 // const now = Date.parse("31 Mar 2022 00:00:00 GMT");
@@ -12,7 +12,7 @@ console.log("Quarterly Clock");
 // const now = Date.parse("01 May 2022 00:00:00 GMT");
 // const now = Date.parse("30 Jun 2022 00:00:00 GMT");
 // const now = Date.parse("01 Jul 2022 00:00:00 GMT");
-const now = Date.parse("01 Aug 2022 00:00:00 GMT");
+// const now = Date.parse("01 Aug 2022 00:00:00 GMT");
 // const now = Date.parse("30 Sep 2022 00:00:00 GMT");
 // const now = Date.parse("01 Oct 2022 00:00:00 GMT");
 // const now = Date.parse("01 Nov 2022 00:00:00 GMT");
