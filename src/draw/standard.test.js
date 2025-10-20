1db9e280f868e6491c7c2b05062ec55c1b852fc0
Mike Moran <mike@houseofmoran.com>
2025-10-20 21:23:07 +0100

    add `defaultArcGenerator` test

--- src/draw/standard.test.js
+++ src/draw/standard.test.js
@@ -1,4 +1,4 @@
-import { defaultFontSizeFormat, defaultPositionRounding } from "./standard";
+import { defaultFontSizeFormat, defaultPositionRounding, defaultArcGenerator } from "./standard";
 import { describe, test, expect } from 'vitest';
 
 test("defaultFontSizeFormat", () => {
@@ -9,4 +9,17 @@ test("defaultFontSizeFormat", () => {
 test("defaultPositionRounding", () => {
   expect(defaultPositionRounding(12)).toBe(12.0);
   expect(defaultPositionRounding(12.345)).toBe(12.35);
+});
+
+test("defaultArcGenerator", () => {
+  const arcGenerator = defaultArcGenerator();
+  expect(arcGenerator.digits()).toBe(3);
+
+  arcGenerator.innerRadius(0).outerRadius(100);
+  const path = arcGenerator({
+    startAngle: 0,
+    endAngle: Math.PI / 2,
+  });
+  expect(path).toBeDefined();
+  expect(path).toBe("M0,-100A100,100,0,0,1,100,0L0,0Z");
 });
