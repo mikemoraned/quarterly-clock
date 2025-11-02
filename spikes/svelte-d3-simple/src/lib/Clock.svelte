ee6e6072a34d9698a8dceba18bd318598b163491
Mike Moran <mike@houseofmoran.com>
2025-11-02 15:33:18 +0000

    simple svg placement using css

---
+++ spikes/svelte-d3-simple/src/lib/Clock.svelte
@@ -0,0 +1,17 @@
+<svg viewBox="-500 -500 1000 1000" preserveAspectRatio="xMidYMid meet">
+	<circle cx="0" cy="0" r="10" class="guide"></circle>
+</svg>
+
+<style>
+	svg {
+		width: min(100vw, 100vh);
+		height: min(100vw, 100vh);
+		border: 1px solid black;
+	}
+
+	circle.guide {
+		fill: red;
+		stroke: black;
+		stroke-width: 2;
+	}
+</style>
--- spikes/svelte-d3-simple/src/routes/+page.svelte
+++ spikes/svelte-d3-simple/src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import Clock from '$lib/Clock.svelte';
+</script>
+
+<Clock />
+
+<style>
+	:global(html),
+	:global(body) {
+		margin: 0;
+		width: 100vw;
+		height: 100vh;
+	}
+	:global(body) {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
