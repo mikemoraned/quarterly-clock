b89633fa9d9678a7fcbe29d007be6c216c8598ea
Mike Moran <mike@houseofmoran.com>
2025-11-07 16:16:39 +0000

    update counter once a second, on average

--- spikes/solidclock/src/clock.tsx
+++ spikes/solidclock/src/clock.tsx
@@ -1,6 +1,7 @@
 import './clock.css';
 import { Hand } from './hand';
 import type { Guides } from './guides';
+import { createSignal, onMount, onCleanup } from 'solid-js';
 
 export const Clock = () => {
     const viewBoxWidth = 1000;
@@ -10,8 +11,19 @@ export const Clock = () => {
         outerRadius: 480
     };
 
+    const [seconds, setSeconds] = createSignal(0);
+    onMount(() => {
+        const interval = setInterval(() => {
+            setSeconds(c => (c + 1) % 60);
+        }, 1000);
+
+        onCleanup(() => {
+            clearInterval(interval);
+        });
+    });
+
     return (<svg class="clock" viewBox={`-${viewBoxWidth / 2} -${viewBoxHeight / 2} ${viewBoxWidth} ${viewBoxHeight}`} preserveAspectRatio="xMidYMid meet">
         <circle cx="0" cy="0" r={guidesModel.outerRadius} stroke="black" stroke-width="10" fill="white" />
-        <Hand guides={guidesModel} elapsedFraction={0.5} />
+        <Hand guides={guidesModel} elapsedFraction={seconds() / 60} />
     </svg>);
 };
