b38324fd38bb0fd4ab1019b46a714545bacbaf0d
Mike Moran <mike@houseofmoran.com>
2025-11-08 15:45:53 +0000

    ignore error on `digits` type

--- spikes/solidclock/src/standard.ts
+++ spikes/solidclock/src/standard.ts
@@ -1,5 +1,6 @@
 import { arc } from "d3-shape";
 
 export function defaultArcGenerator() {
+    // @ts-expect-error: digits exists at runtime but is missing from d3-shape typings
     return arc().digits(1);
 }
