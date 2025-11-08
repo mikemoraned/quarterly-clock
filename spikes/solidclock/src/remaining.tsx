a6c0b4d173d0def243e157ba632d5c014f6f3f43
Mike Moran <mike@houseofmoran.com>
2025-11-08 13:23:11 +0000

    rearrange code for arc to make types happy

--- spikes/solidclock/src/remaining.tsx
+++ spikes/solidclock/src/remaining.tsx
@@ -4,12 +4,12 @@ import { Guides } from "./guides";
 export const Remaining = (props: { guides: Guides, elapsedFraction: number }) => {
     const { outerRadius } = props.guides;
 
-    const arcGenerator = arc()
-        .innerRadius(outerRadius / 3)
-        .outerRadius(outerRadius - outerRadius / 15);
+    const arcGenerator = arc();
 
     const d = () => {
         return arcGenerator({
+            innerRadius: outerRadius / 3,
+            outerRadius: outerRadius - outerRadius / 15,
             startAngle: props.elapsedFraction * 2.0 * Math.PI,
             endAngle: 2.0 * Math.PI
         });
