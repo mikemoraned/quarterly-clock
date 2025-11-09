763685e1f5efb4d89c80307fb8e336f2ff8d3b58
Mike Moran <mike@houseofmoran.com>
2025-11-09 00:20:04 +0000

    change direction of arc used for textPath based on whether it is in bottom or top of circle

--- next2025/quarterly-clock/src/gutter-names.tsx
+++ next2025/quarterly-clock/src/gutter-names.tsx
@@ -3,7 +3,8 @@ import { useGuides } from "./guides-provider";
 import { Interval, OrderedIntervals } from "./model/months";
 import { defaultArcGenerator } from "./standard";
 import { For } from "solid-js";
-import { scaleTime } from "d3-scale";
+import { scaleTime, scaleQuantize } from "d3-scale";
+
 
 export function GutterIntervalNames(props: { intervals: OrderedIntervals }) {
     const guides: Guides = useGuides()!;
@@ -13,14 +14,29 @@ export function GutterIntervalNames(props: { intervals: OrderedIntervals }) {
         .domain([props.intervals.limits.start, props.intervals.limits.end])
         .range([0, 2 * Math.PI]);
 
+    const segmentScale = scaleQuantize<string>()
+        .domain([0, 2 * Math.PI])
+        .range(["TopRight", "BottomRight", "BottomLeft", "TopLeft"]);
+
     const pathForInterval = (interval: Interval): string => {
-        return arcGenerator({
-            innerRadius: guides.gutterRadius,
-            outerRadius: guides.gutterRadius,
-            startAngle: timeScale(interval.start),
-            endAngle: timeScale(interval.end)
-        })!;
-    }
+        const segment = segmentScale(timeScale(interval.end));
+        if (segment === "TopRight" || segment === "TopLeft") {
+            return arcGenerator({
+                innerRadius: guides.gutterRadius,
+                outerRadius: guides.gutterRadius,
+                startAngle: timeScale(interval.start),
+                endAngle: timeScale(interval.end)
+            })!;
+        }
+        else {
+            return arcGenerator({
+                innerRadius: guides.gutterRadius,
+                outerRadius: guides.gutterRadius,
+                startAngle: timeScale(interval.end),
+                endAngle: timeScale(interval.start)
+            })!;
+        }
+    };
 
     const id = (i: number) => `gutter-interval-names-arcs-${i}`;
 
