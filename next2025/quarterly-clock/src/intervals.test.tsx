6b41c0071fb31d06f0ef9277a47f0cfc5578a1d2
Mike Moran <mike@houseofmoran.com>
2025-11-14 21:58:49 +0000

    add Intervals snapshot test

---
+++ next2025/quarterly-clock/src/intervals.test.tsx
@@ -0,0 +1,36 @@
+import { test, expect } from "vitest";
+import { render } from "@solidjs/testing-library";
+import { GuidesProvider } from "./guides-provider";
+import { JSX } from "solid-js";
+import { ViewBox } from "./svg/view-box";
+import { QuarterSpecification } from "./model/quarterSpecification";
+import { quarterIntervals } from "./model/quarters";
+import { Quarters } from "./quarters";
+import { monthIntervals } from "./model/months";
+import { Intervals } from "./intervals";
+
+const VIEW_BOX: ViewBox = {
+    width: 1000,
+    height: 1000
+};
+
+const wrapper = (props: { children: JSX.Element }) => <svg>
+    <GuidesProvider viewBox={VIEW_BOX}>
+        {props.children}
+    </GuidesProvider>
+</svg>;
+
+test("calendar year is 2021, and quarters start on January 1st", async () => {
+    const quarterSpecification = QuarterSpecification.default();
+    const intervals = monthIntervals(2021, quarterSpecification);
+    const { container } = render(() => <Intervals intervals={intervals} gradientId="dummy_gradient" />, { wrapper });
+    expect(container).toMatchSnapshot();
+});
+
+test("calendar year is 2021, and quarters start on July 1st", () => {
+    const quarterSpecification = new QuarterSpecification(6);
+    const intervals = monthIntervals(2021, quarterSpecification);
+    const { container } = render(() => <Intervals intervals={intervals} gradientId="dummy_gradient" />, { wrapper });
+    expect(container).toMatchSnapshot();
+});
+
