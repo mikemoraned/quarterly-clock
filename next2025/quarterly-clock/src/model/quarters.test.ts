3503c773b96615e1c2e7f4ccad9bab9ec61a47c3
Mike Moran <mike@houseofmoran.com>
2026-03-11 21:00:03 +0000

    add some failing tests for `quarterIntervalForDate`
    - these capture that when a quarter starts part way through the year, if our month is before that, then we are still in previous year wrt quarters

--- next2025/quarterly-clock/src/model/quarters.test.ts
+++ next2025/quarterly-clock/src/model/quarters.test.ts
@@ -1,9 +1,7 @@
 import { describe, expect, test } from "vitest";
-import { monthStarts } from "./months";
-import { monthIntervals } from "./months";
 import { QuarterSpecification } from "./quarterSpecification";
 import { Interval } from "./intervals";
-import { quarterIntervals, quarterStarts } from "./quarters";
+import { quarterIntervalForDate, quarterIntervals, quarterStarts } from "./quarters";
 
 describe("calendar year is 2021, and quarters start on January 1st", () => {
     const quarterSpecification = QuarterSpecification.default();
@@ -52,6 +50,39 @@ describe("calendar year is 2021, and quarters start on July 1st", () => {
         ];
         expectEqualIntervals(expectedIntervals, intervals.intervals);
         expect(intervals.limits).toEqual({ start: new Date(2021, 6, 1, 0, 0, 0, 0), end: new Date(2022, 5, 30, 23, 59, 59, 999) });
+    });
+});
+
+describe("quarter interval for dates, when quarters start on January 1st", () => {
+    const quarterSpecification = QuarterSpecification.default();
+    test("a date before January", () => {
+        // Nov is in Q4
+        const date = new Date(2020, 10, 1, 0, 0, 0, 0);
+        const quarter = quarterIntervalForDate(date, quarterSpecification);
+        expect(quarter).toEqual({ start: new Date(2020, 9, 1, 0, 0, 0, 0), end: new Date(2020, 11, 31, 23, 59, 59, 999), name: "Q4" });
+    });
+    test("a date after January", () => {
+        // March is in Q1
+        const date = new Date(2021, 3, 1, 0, 0, 0, 0);
+        const quarter = quarterIntervalForDate(date, quarterSpecification);
+        expect(quarter).toEqual({ start: new Date(2021, 0, 1, 0, 0, 0, 0), end: new Date(2021, 2, 31, 23, 59, 59, 999), name: "Q1" });
+    });
+});
+
+
+describe("quarter interval for dates, when quarters start on July 1st", () => {
+    const quarterSpecification = new QuarterSpecification(6);
+    test("a date before July", () => {
+        // May is in Q4
+        const date = new Date(2021, 4, 1, 0, 0, 0, 0);
+        const quarter = quarterIntervalForDate(date, quarterSpecification);
+        expect(quarter).toEqual({ start: new Date(2021, 3, 1, 0, 0, 0, 0), end: new Date(2021, 5, 30, 23, 59, 59, 999), name: "Q4" });
+    });
+    test("a date after July", () => {
+        // August is in Q1
+        const date = new Date(2021, 7, 1, 0, 0, 0, 0);
+        const quarter = quarterIntervalForDate(date, quarterSpecification);
+        expect(quarter).toEqual({ start: new Date(2021, 6, 1, 0, 0, 0, 0), end: new Date(2021, 8, 30, 23, 59, 59, 999), name: "Q1" });
     });
 });
 
